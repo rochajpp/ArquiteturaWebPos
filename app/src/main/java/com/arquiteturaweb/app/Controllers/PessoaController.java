304c6fd6f5099d6b8eea4d3843d9140cf884cf7e
João Paulo Rocha <joao.rochap03@gmail.com>
2026-05-24 18:39:02 -0300

    :sparkles: feat: Adiciona rota de update de pessoa

--- app/src/main/java/com/arquiteturaweb/app/Controllers/PessoaController.java
+++ app/src/main/java/com/arquiteturaweb/app/Controllers/PessoaController.java
@@ -1,13 +1,16 @@
 package com.arquiteturaweb.app.Controllers;
 
-import java.net.http.HttpResponse;
 import java.util.List;
 
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arquiteturaweb.app.Repositories.PessoaRepository;
@@ -15,6 +18,7 @@ import com.arquiteturaweb.app.Repositories.PessoaRepository;
 import com.arquiteturaweb.app.Entities.Pessoa;
 
 @RestController
+@RequestMapping("/pessoa")
 public class PessoaController {
 
     private PessoaRepository Context;
@@ -37,9 +41,6 @@ public class PessoaController {
     @PostMapping("/save")
     public ResponseEntity<String> save(@RequestBody Pessoa pessoa){
         Pessoa p = new Pessoa(pessoa.getNome(), pessoa.getIdade());
-        System.out.println(p.getNome());
-        System.out.println(p.getIdade());
-        System.out.println(p.getId());
 
         try{
             Context.save(p);
@@ -48,6 +49,27 @@ public class PessoaController {
             System.out.println(e);
             return ResponseEntity.badRequest().build();
         }
+    }
 
+    @PutMapping("/update")
+    public ResponseEntity<String> update(@RequestBody Pessoa pessoa){
+        Optional<Pessoa> p = Context.findById(pessoa.getId());
+        
+        if(!p.isPresent()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Pessoa com o ID: " + pessoa.getId() + " não encontrado");
+        }
+
+        Pessoa pessoaUpdate = p.get();
+
+        pessoaUpdate.setNome(pessoa.getNome());
+        pessoaUpdate.setIdade(pessoa.getIdade());
+        
+        try{
+            Context.save(pessoaUpdate);
+            return ResponseEntity.ok("Pessoa atualizada com sucesso");
+        } catch(Exception e){
+            System.out.println(e);
+            return ResponseEntity.badRequest().body("Erro ao persistir no banco");
+        }
     }
 }
